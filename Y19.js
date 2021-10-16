ae71cd9c3016966225b14e9ae6396ed5190a4ae0
patronus-charm <85407405+patronus-charm@users.noreply.github.com>
2021-10-16 17:05:32 +0530

    Add files via upload

---
+++ Y19.js
@@ -0,0 +1,148 @@
+var data=[
+    {
+        id: 1,
+		filepath:'akashpatel.html',
+		name: 'Akash Patel',
+		image: '../../../assets/img/senators19/Akash Patel.jpg',
+		tagLine: 'Come Together-Solve Together-Grow Together',
+		post: 'Senator Y19',
+		about: "I am passionate and hard working in my work and eager to perform well on assigned tasks. I believe right guidance at right time is very important for career of students. Commitment and fulfilling promises have been always my first priority.",
+        manifesto: 'https://drive.google.com/file/d/1ZW5m-GDsFHF0ew-XYS5uBR535RGVqv-M/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Akash patel.jpg'],
+
+    },
+    {
+        id: 2,
+		filepath:'akshatgoyal.html',
+		name: 'Akshat Goyal',
+		image: '../../../assets/img/senators19/Akshat_Goyal.jpeg',
+		tagLine: 'Goyal_For_Senate',
+		post: 'Senator Y19',
+		about: "Hi I’m Akshat Goyal! I am a Y19 BT MSE student and am contesting for the position of Senator BT/BS Y19. You would have seen me at the Lawn Tennis courts in the evenings and the Hall 13 canteen at 1:45 AM without fail xD. I vision this tenure to be one of restoring normalcy and reunion of us the student community and hence declare my candidature to be at the forefront of this process for our batch. Hope to see you all around soon and Vote responsibly!",
+        manifesto: 'https://drive.google.com/file/d/1cf5HBOexwPXlOUcn-xAngbpfazO3e7Uj/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Akshat goyal.jpg'],
+
+    },
+    {
+        id: 3,
+		filepath:'akshayjain.html',
+		name: 'Akshay Jain',
+		image: '../../../assets/img/senators19/AKSHAY JAIN.jpg',
+		tagLine: '#Akshay_For_Betterment',
+		post: 'Senator Y19',
+		about: "My name is Akshay Jain, a Y19 undergrad contesting for the BT/BS senator elections. My past contributions were in takneek and fests in the offline years. I want to represent my batch and do the things that will eventually ends up in the betterment of them.", 
+        manifesto: 'https://drive.google.com/file/d/1dNJsu2x90yj5aG4T7rgTAOKVpyau7XRn/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Akshay jain.jpg'],
+
+    },
+    {
+        id: 4,
+		filepath:'devanshparmar.html',
+		name: 'Devansh Parmar',
+		image: '../../../assets/img/senators19/Devansh Parmar.png',
+		tagLine: '#ParmarForSenate',
+		post: 'Senator Y19',
+		about: "Hi, I’m Devansh Parmar, a Y19 BT EE student. I’ve been an active member of the Students' Senate since my first semester, and hope to do so in the coming semesters too. As you might know, it requires experience to know the functions and working of the Senate, so I hope that I am well-suited to contest and continue for the post of Senator for my batch and look forward to work in the interest of it yet again. Vote Wisely!",
+        manifesto: 'https://drive.google.com/file/d/1Dn5ZfUzpS32K7NpJL1LbPVLOO0I9Lm3l/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Devansh parmar.jpg'],
+
+    },
+    {
+        id: 5,
+		filepath:'Gurbaazsinghnandra.html',
+		name: 'Gurbaaz Singh Nandra',
+		image: '../../../assets/img/senators19/Gurbaaz Singh Nandra.jpeg',
+		tagLine: '#fly_with_baaz',
+		post: 'Senator Y19',
+		about: "Greetings to everyone! I am Gurbaaz Singh Nandra, a sophomore and prospective candidate for Senator BT/BS Y19 in the upcoming General Elections 2021. The Senate, as you already know, is of, by, and for the students of campus community itself. A senator acts as a mediator between students and Senate and makes your voice feel present in the decision making process. From my end, I ensure to work in collaboration with the Students' Gymkhana and address your issues, hence coming up with feasible, scalable and acceptable solutions. Stay happy and blessed!", 
+        manifesto: 'https://drive.google.com/file/d/1ZEK9YAeS79ORblC9hZOtu_d-4qzb7gLO/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Gurbaaz.png','../../../assets/img/Posters/Gurbaaz(2).png'],
+
+    },
+    {
+        id: 6,
+		filepath:'maryamkhan.html',
+		name: 'Maryam Khan',
+		image: '../../../assets/img/senators19/Maryam Khan.jpg',
+		tagLine: '#YourVoteForYou',
+		post: 'Senator Y19',
+		about: "I am Maryam , a Mechanical sophomore.Here I am before you , considerate as I am, my thoughts genuine as they are .I'd rather not make any false promises or put up impossible models. I believe in Deeds not Words, Plans not Dreams, and always You before Me !", 
+        manifesto: 'https://drive.google.com/file/d/1rA3t0eXjgPeXfaql0C9gMLBm-BYdrbcj/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Maryam.jpg','../../../assets/img/Posters/Maryam(2).jpg'],
+
+    },
+    {
+        id: 7,
+		filepath:'rishikasaraswat.html',
+		name: 'Rishika Saraswat',
+		image: '../../../assets/img/senators19/RishikaSaraswat.jpg',
+		tagLine: '#Rishika_for_Senate',
+		post: 'Senator Y19',
+		about: "I am a Y19 who is motivated to represent the interests of her batch in the Senate and bring about effective changes. I will strive towards increasing transparency and accountability in the Students' Senate and will keep the interests of my batchmates at a greater priority than mine.", 
+        manifesto: 'https://drive.google.com/file/d/1r-mphaiEHetQCYmrIdUyu6X-AwQYFO9h/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Rishika.png'],
+
+    },
+    {
+        id: 8,
+		filepath:'rohitkejriwal.html',
+		name: 'Rohit Kejriwal',
+		image: '../../../assets/img/senators19/Rohit Kejriwal.jpeg',
+		tagLine: '#VoteToRevamp',
+		post: 'Senator Y19',
+		about: "I am Rohit Kejriwal, an anime-loving Mathematics sophomore. A firm believer that our institute has a lot to offer us, and if student representatives act together as a team, we can make things happen.", 
+        manifesto: 'https://drive.google.com/file/d/1IBaczQOhGx5bH-To7s3RJNLyy4L_Md4L/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Rohit kejriwal.jpg'],
+
+    },
+    {
+        id: 9,
+		filepath:'sanidhyarathore.html',
+		name: 'Sanidhya Rathore',
+		image: '../../../assets/img/senators19/Sanidhya Rathore.png',
+		tagLine: '#sanu_for_senate',
+		post: 'Senator Y19',
+		about: "Hi, I'm Sanidhya Rathore! many people know me as SANU :) A Y19 student in the Department of Electrical Engineering. I'm contesting for BT/BS Y19. I bet you have seen me many times enjoying my TEA at various food joints in the campus. I see this tenure to be full of alien things and is pretty unpredictable. I can certainly guarantee you to be completely accountable, and always open and approachable, to you and your problems as individuals, and batch as a whole. If not then we can always have a 'CHAI PE CHARCHA'.",
+        manifesto: 'https://drive.google.com/file/d/1p3jisHpwm9HVmV0SOOAKkWNETgfZQfRJ/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Sanidhya.jpg'],
+
+    },
+    {
+        id: 10,
+		filepath:'sauravsuman.html',
+		name: 'Saurav Suman',
+		image: '../../../assets/img/senators19/Saurav Suman.jpg',
+		tagLine: 'Saurav for senator.',
+		post: 'Senator Y19',
+		about: "I am dedicated, open minded man. I thrive on challenge and constantly set goals for myself. I am always looking for an opportunity to do better.",
+        manifesto: 'https://drive.google.com/file/d/1MP6rR2Ws4whLcCbzmZExjV5cQcYhYIW1/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Saurav suman.jpg'],
+
+    },
+    {
+        id: 11,
+		filepath:'shiventripathi.html',
+		name: 'Shiven Tripathi',
+		image: '../../../assets/img/senators19/Shiven Tripathi.png',
+		tagLine: 'Ask not what IITK can do for you - ask what you can do for IITK',
+		post: 'Senator Y19',
+		about: "As a student senator, I won't attempt to promise something which would remain a poll gimmick. Still, I can promise that I will do everything possible to ensure that the Students' Senate is doing everything they can to help recognise our batch's issues. Having served in various capacities in various student bodies like the Senate, Debating Society etc., I have realised the difference a clear vision for execution can bring forth, the same which would prove to be useful to bring forth meaningful change through consistent action as a senator.", 
+        manifesto: 'https://drive.google.com/file/d/1Bxqj7UdbXLcJG2gcOQ86pq1hKON_CkvQ/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Shiven tripathi.png'],
+
+    },
+    {
+        id: 12,
+		filepath:'siddharthgupta.html',
+		name: 'Siddharth Gupta',
+		image: '../../../assets/img/senators19/siddharthgupta.jpg',
+		tagLine: '#SiddForSenator',
+		post: 'Senator Y19',
+		about: "I am sophomore undergraduate majoring in BSBE department. I believe that I’m not a perfectionist, but I like to feel that things are done well and the same thinking I will apply in my tenure as a senator.",
+        manifesto: 'https://drive.google.com/file/d/1_JUYBvtGIDcX4a4bS35WYZXXD9OGEIpF/view?usp=sharing',
+		poster: ['../../../assets/img/Posters/Sidharth gupta.png'],
+
+    },
+
+];
+export default data;
