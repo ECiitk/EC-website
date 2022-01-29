253be17eaf656b70cd52af69570a6a6290f48b64
patronus-charm <85407405+patronus-charm@users.noreply.github.com>
2022-01-29 14:52:59 +0530

    New Candidates added

---
+++ candidates/Senators/Y21/datasenatory21.js
@@ -0,0 +1,233 @@
+var data=[
+    {
+        "id":1,
+        "name":"Aatman Jain",
+        "image":"../../../assets/img/senators21/Y21 Senator_Aatman_Jain.jpg",
+        "tagLine":"#ManSeAatman",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1Ut1g7HQS0ydv1hQHGX1vSx3pFj1_4qJ9/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Aatman.jpg"]
+
+    },
+
+    {
+        "id":2,
+        "name":"Abhiraj Akhouri",
+        "image":"../../../assets/img/senators21/BT_BS Y21 Senator _ Abhiraj_Akhouri.jpg",
+        "tagLine":"#IAmOneOfYou",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1PXV2tnaq2kf47pbj04QgKnoiNXCDw-Yx/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Abhiraj.jpg"]
+    },
+
+    {
+        "id":3,
+        "name":"Aman Pratap Yadav",
+        "image":"../../../assets/img/senators21/Y21Senator_Aman_Yadav.png",
+        "tagLine":"#Aman_for_senator",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1f-Qu8-UE1NPA_4sKnSy5-fpgA5S_5g3l/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Aman.jpg"]
+    },
+
+    {
+        "id":4,
+        "name":"Deekshansh Vardhan",
+        "image":"../../../assets/img/senators21/BT_BS senator Y21_Deekshansh_Vardhan.jpg",
+        "tagLine":"#deekshansh_for_senator",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1qb07tsRTAcUtQirDdVuoPXNRUGuT0cw1/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Deekshansh.jpg"]
+    },
+
+    {
+        "id":5,
+        "name":"Jyotirmay Singh",
+        "image":"../../../assets/img/senators21/BT_BS Y21 Senator Jyotirmay.jpg",
+        "tagLine":"#",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1ddVZ4T9Cv2gHQ7gnam2kBuOQreVKfn-f/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Jyotirmay.jpg"]
+    },
+
+    {
+        "id":6,
+        "name":"Kushagra Gupta",
+        "image":"../../../assets/img/senators21/BT_BS Senator Y21_ Kushagra Gupta.jpg",
+        "tagLine":"#KUSHAGRA_HAI_EKAGRA",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/192ogq8s3lW5kyPgPk-Y-JiKWVdaYy-Gc/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Kushagra.jpg"]
+    },
+
+    {
+        "id":7,
+        "name":"Prasang Vashishtha",
+        "image":"../../../assets/img/senators21/Senator BT_BS Y21_ Prasang Vashishtha.jpg",
+        "tagLine":"TakingActionGettingResults",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1evyqw9RHjGWyYM-yGzbyAX1xs1uibUSg/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Prasang.jpg"]
+    },
+
+    {
+        "id":8,
+        "name":"Rahul Jha",
+        "image":"../../../assets/img/senators21/Senator BS-BS Y21_Rahul_Jha.jpeg",
+        "tagLine":"#",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1KKp_eFANybhhQ0Wv8GREklAwr4TdeaxJ/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Rahul.jpg"]
+    },
+
+    {
+        "id":9,
+        "name":"Rachit Gupta",
+        "image":"../../../assets/img/senators21/Senator BT.BS Y21_Rachit_gupta.jpeg",
+        "tagLine":"#",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1xnPF82Z48BO1S7CWB8qUH8MTOWSDQcb2/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Rachit.jpg"]
+    },
+
+    {
+        "id":10,
+        "name":"Ritam Acharya",
+        "image":"../../../assets/img/senators21/SENATOR BT BS Y21_Ritam_Acharya.jpg",
+        "tagLine":"#Ritam_for_reform",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1rAzxGDfxy-EswEbNv3II2kvXl8Esm3tI/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Ritam.jpg"]
+    },
+
+    {
+        "id":11,
+        "name":"Shreyansh Kesharwani",
+        "image":"../../../assets/img/senators21/SENATOR_BT_BS_Y21_SHREYANSH_KESHARWANI.jpeg",
+        "tagLine":"#shreyansh_for_senator",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1nCNBP3nWOLOAGxH7Iqw5L-ENykpMxYwt/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Shreyansh.jpg"]
+    },
+
+    {
+        "id":12,
+        "name":"Sahil Panjwani",
+        "image":"../../../assets/img/senators21/SENATORY21_SAHIL_PANJWANI.jpg",
+        "tagLine":"#",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1-kx-RcooCyL-FQbxCcVJsNAvQx3g2Gs8/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Sahil.jpg"]
+    },
+
+    {
+        "id":13,
+        "name":"Srijan Kumar",
+        "image":"../../../assets/img/senators21/SENATOR Y21 BT_BS_SRIJAN_KUMAR.jpg",
+        "tagLine":"#Together_we_win",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1zTfFsIUbW5LoONzXy-bGxF8uaL6hudpZ/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Srijan.jpg"]
+    },
+
+    {
+        "id":14,
+        "name":"Talin Gupta",
+        "image":"../../../assets/img/senators21/Talin Gupta.jpg",
+        "tagLine":"#yourforeverkin_talin",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1a7_W3cedlkwM87xN1L8wEqpRx0VziVBm/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Talin.jpg"]
+    },
+
+    {
+        "id":15,
+        "name":"Tanmay Purohit",
+        "image":"../../../assets/img/senators21/Senator BT_BS Y21_Tanmay_Purohit.jpeg",
+        "tagLine":"#",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1DUXq6yJXByH7Ea_VpBo_YQjQuJxdRZKR/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Tanmay.jpg"]
+    },
+
+    {
+        "id":16,
+        "name":"Akansha Patel",
+        "image":"../../../assets/img/senators21/Y21 BT_BS Senator_Akansha_Patel.jpg",
+        "tagLine":"#YourVoteYourVoice",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1uPs9oshkkceDBbIr4sLvbmhFpU30U1xr/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Akansha.jpg"]
+    },
+
+    {
+        "id":17,
+        "name":"Himanshu Nagar",
+        "image":"../../../assets/img/senators21/BT_BS Y21_Himanshu_Nagar.jpg",
+        "tagLine":"#choose_right_choose_himanshu",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1qBZqxVeCtjgh_jVycV5Yp9x0q3hVLu11/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Himanshu.jpg"]
+    },
+
+    {
+        "id":18,
+        "name":"Nitin Garhwal",
+        "image":"../../../assets/img/senators21/Y21 Senator_Nitin_Garhwal.jpg",
+        "tagLine":"#nitin_for_y21",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1mUmUpIoPoAsesG3vJ-d22xVhBlWen_Cg/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Nitin.jpg"]
+    },
+
+    {
+        "id":19,
+        "name":"Prassun Duggar",
+        "image":"../../../assets/img/senators21/Senator_Y21_Prassun_Duggar.jpeg",
+        "tagLine":"VOTE_FOR_PRASSUN",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/10t9m0USaNkeo6SJWOOvcq8e3xxwwZlGv/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Prassun.jpg"]
+    },
+
+    {
+        "id":20,
+        "name":"Vilakshan Vijayvargiya",
+        "image":"../../../assets/img/senators21/SenatorY21_Vilakshan_Vijayvargiya.jpg",
+        "tagLine":"#",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1A4xp2pahvSvD6oTmzil_tZSIYEy9pgps/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Vilakshan.jpg"]
+    },
+
+    {
+        "id":21,
+        "name":"Sharvani Jadhav",
+        "image":"../../../assets/img/senators21/BT_BS Y21 Senator Sharvani Jadhav.jpeg",
+        "tagLine":"#sharvani_speaks",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/12sznnv4Zv5lXidPL9Y5C65gFDKR2oKCX/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Sharvani.jpg"]
+    },
+
+    {
+        "id":22,
+        "name":"Supritum Karmakar",
+        "image":"../../../assets/img/senators21/Supritum Karmakar.jpg",
+        "tagLine":"Y21_demands_teamSUPRITUM_supplies",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1JBb7VXPWWKxKTs560gvOjCqxzJMtj7SE/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Supritum.jpg"]
+    },
+
+    {
+        "id":23,
+        "name":"Prachi Choudhary",
+        "image":"../../../assets/img/senators21/prachi.jpg",
+        "tagLine":"#PrachiforSenate",
+        "post":"Senator BT/BS Y21",
+        "manifesto":"https://drive.google.com/file/d/1WlW1P2tj1vuzvNA-Ro33N7KX1Uv8FcsJ/view?usp=sharing",
+        "poster":["../../../assets/img/Posters/Prachi.jpg"]
+    }
+];
+export default data;
---
+++ candidates/data.js
@@ -0,0 +1,60 @@
+var data = [
+    {
+		id: 1,
+		name: 'Divyesh Choudhary',
+		image: '../assets/img/executive_candidate/Divyesh Choudhary.jpg',
+		tagLine: '#Difference_with_Divyesh',
+		post: 'President, Students’ Gymkhana',
+		manifesto: 'https://drive.google.com/file/d/15dWUhqlJ-hWz4ooJA4AoMBp_rRxu6x6a/view?usp=sharing',
+		poster: ['../'],
+	},
+	{
+		id: 2,
+		name: 'Rohit Kejriwal',
+		image: '../assets/img/executive_candidate/Rohit Kejriwal.jpeg',
+		tagLine: '#rely_recover_revamp',
+		post: 'President, Students’ Gymkhana',
+		manifesto: 'https://drive.google.com/file/d/1AQvEHZ26kRiCbJS26g_auBEaYRgCXScR/view?usp=sharing',
+		poster: ['../assets/img/Posters/Rohit.jpg'],
+	},
+	{
+		id: 3,
+		name: 'Animesh Singh',
+		image: '../assets/img/executive_candidate/Animesh Singh.png',
+		tagLine: '#culture_wont_die',
+		post: 'General Secretary, Games and Sport',
+		manifesto: 'https://drive.google.com/file/d/1W4IDJDkNGkVGe_o1v70-XqM_KQGf3Mw9/view?usp=sharing',
+		poster: ['../assets/img/Posters/Animesh.jpg'],
+	},
+	{
+		id: 4,
+		name: 'Manit Ajmera',
+		image: '../assets/img/executive_candidate/Manit Ajmera.png',
+		tagLine: '#Ajmera_for_AnC',
+		post: 'General Secretary, PG Academics and Career',
+		manifesto: 'https://drive.google.com/file/d/1k6A_nfmOT8xNnVGNNy0AzrYlzl5DP5Lo/view?usp=sharing',
+		poster: ['../'],
+	},
+	
+	{
+		id: 5,
+		name: 'Ghanshyam Waindeshkar',
+		image: '../assets/img/executive_candidate/Ghanshyam Waindeshkar.jpeg',
+		tagLine: '#revive_the_glorious_past',
+		post: 'General Secretary, Media and Culture',
+		manifesto: 'https://drive.google.com/file/d/1oixPOrMZ9oFxudLUKQalpB1dZEnP_XTg/view?usp=sharing',
+		poster: ['../'],
+	},
+	
+	{
+		id: 6,
+		name: 'Som Tambe',
+		image: '../assets/img/executive_candidate/Som Tambe.JPG',
+		tagLine: '#TamberForSnT',
+		post: 'General Secretary, Science and Technology',
+		manifesto: 'https://drive.google.com/file/d/1_C-w2xzAqMUL-uc8nwJKhkhA3AoC7-MV/view?usp=sharing',
+		poster: ['../'],
+	}
+];
+
+export default data;
