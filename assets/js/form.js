16fae99891cd6f312c3f50a107562beae479d0d9
patronus-charm <85407405+patronus-charm@users.noreply.github.com>
2022-08-16 19:07:06 +0530

    .

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,8 +4,8 @@ var message=document.getElementbyid("message").value;
 var url=`https://us-central1-mailer-64d8d.cloudfunctions.net/sendMail3?name=?${name}&email=${email}&mess=${message}`;
 fetch(url)
 .then((response) => {
-alert('Mail Sent);
+alert('Mail Sent');
 })
 .catch((error) => {
-alert('Error);
+alert('Error');
 });
